4601014a5fa120938efe79e43b34c7bd0d19f6ac
sebthom <sebthom@users.noreply.github.com>
2022-07-26 18:45:28 +0200

    update copyright notice

--- image/run.sh
+++ image/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2021 by Vegard IT GmbH, Germany, https://vegardit.com
+# Copyright 2021-2022 by Vegard IT GmbH, Germany, https://vegardit.com
 # SPDX-License-Identifier: Apache-2.0
 #
 # Author: Sebastian Thomschke, Vegard IT GmbH
