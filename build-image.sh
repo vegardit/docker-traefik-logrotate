1767de1dc69b6342df711c4794b22d82ad54b44c
sebthom <sebthom@users.noreply.github.com>
2025-05-16 20:34:44 +0200

    build: update build config

--- build-image.sh
+++ build-image.sh
@@ -9,15 +9,25 @@ function curl() {
   command curl -sSfL --connect-timeout 10 --max-time 30 --retry 3 --retry-all-errors "$@"
 }
 
-shared_lib="$(dirname $0)/.shared"
+shared_lib="$(dirname "${BASH_SOURCE[0]}")/.shared"
 [ -e "$shared_lib" ] || curl https://raw.githubusercontent.com/vegardit/docker-shared/v1/download.sh?_=$(date +%s) | bash -s v1 "$shared_lib" || exit 1
 source "$shared_lib/lib/build-image-init.sh"
 
 
 #################################################
-# specify target docker registry/repo
+# check prereqs
 #################################################
-docker_registry=${DOCKER_REGISTRY:-docker.io}
+
+if [[ "${DOCKER_PUSH:-}" == "true" ]]; then
+  if ! hash regctl &>/dev/null; then
+    log ERROR "regctl (aka regclient) command line tool is misssing!"
+  fi
+fi
+
+
+#################################################
+# specify target image repo/tag
+#################################################
 image_repo=${DOCKER_IMAGE_REPO:-vegardit/traefik-logrotate}
 image_name=$image_repo:${DOCKER_IMAGE_TAG:-latest}
 
@@ -31,11 +41,23 @@ if [[ $OSTYPE == "cygwin" || $OSTYPE == "msys" ]]; then
 fi
 
 # https://github.com/docker/buildx/#building-multi-platform-images
+set -x
+
+docker --version
 docker run --privileged --rm tonistiigi/binfmt --install all
-export DOCKER_CLI_EXPERIMENTAL=enabled # prevents "docker: 'buildx' is not a docker command."
-docker buildx create --use # prevents: error: multiple platforms feature is currently not supported for docker driver. Please switch to a different driver (eg. "docker buildx create --use")
+export DOCKER_BUILD_KIT=1
+export DOCKER_CLI_EXPERIMENTAL=1 # prevents "docker: 'buildx' is not a docker command."
+
+# https://docs.docker.com/build/buildkit/configure/#resource-limiting
+echo "
+[worker.oci]
+  max-parallelism = 3
+" | sudo tee /etc/buildkitd.toml
+
+docker buildx version # ensures buildx is enabled
+docker buildx create --config /etc/buildkitd.toml --use # prevents: error: multiple platforms feature is currently not supported for docker driver. Please switch to a different driver (eg. "docker buildx create --use")
 docker buildx build "$project_root" \
-  --file "image/${DOCKER_FILE:-Dockerfile}" \
+  --file "image/Dockerfile" \
   --progress=plain \
   --pull \
   --build-arg INSTALL_SUPPORT_TOOLS=${INSTALL_SUPPORT_TOOLS:-0} \
@@ -46,12 +68,29 @@ docker buildx build "$project_root" \
   --build-arg GIT_COMMIT_DATE="$(date -d @$(git log -1 --format='%at') --utc +'%Y-%m-%d %H:%M:%S UTC')" \
   --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
   --build-arg GIT_REPO_URL="$(git config --get remote.origin.url)" \
-  --platform linux/amd64,linux/arm64,linux/arm/v7 \
-  -t $image_name \
-  $(if [[ "${DOCKER_PUSH:-0}" == "true" ]]; then echo -n "--push"; fi) \
+  $(if [[ "${ACT:-}" == "true" || "${DOCKER_PUSH:-}" != "true" ]]; then \
+    echo -n "--load --output type=docker"; \
+  else \
+    echo -n "--platform linux/amd64,linux/arm64,linux/arm/v7"; \
+  fi) \
+  --tag $image_name \
+  $(if [[ "${DOCKER_PUSH:-}" == "true" ]]; then echo -n "--push"; fi) \
   "$@"
 docker buildx stop
-docker image pull $image_name
+set +x
+
+if [[ "${DOCKER_PUSH:-}" == "true" ]]; then
+  docker image pull $image_name
+fi
+
+
+#################################################
+# test image
+#################################################
+echo
+log INFO "Testing docker image [$image_name]..."
+(set -x; docker run --rm $image_name logrotate --version)
+echo
 
 
 #################################################
@@ -59,4 +98,12 @@ docker image pull $image_name
 #################################################
 if [[ "${DOCKER_AUDIT_IMAGE:-1}" == 1 ]]; then
   bash "$shared_lib/cmd/audit-image.sh" $image_name
+fi
+
+
+#################################################
+# push image to ghcr.io
+#################################################
+if [[ "${DOCKER_PUSH_GHCR:-}" == "true" ]]; then
+  (set -x; regctl image copy $image_name ghcr.io/$image_name)
 fi
