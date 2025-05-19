973b99e0083ea7ecb38398c0b1f9bb02161a90da
sebthom <sebthom@users.noreply.github.com>
2025-05-19 17:03:59 +0200

    build: update build config

--- build-image.sh
+++ build-image.sh
@@ -6,11 +6,12 @@
 # SPDX-ArtifactOfProjectHomePage: https://github.com/vegardit/docker-traefik-logrotate
 
 function curl() {
-  command curl -sSfL --connect-timeout 10 --max-time 30 --retry 3 --retry-all-errors "$@"
+   command curl -sSfL --connect-timeout 10 --max-time 30 --retry 3 --retry-all-errors "$@"
 }
 
 shared_lib="$(dirname "${BASH_SOURCE[0]}")/.shared"
-[ -e "$shared_lib" ] || curl https://raw.githubusercontent.com/vegardit/docker-shared/v1/download.sh?_=$(date +%s) | bash -s v1 "$shared_lib" || exit 1
+[[ -e $shared_lib ]] || curl "https://raw.githubusercontent.com/vegardit/docker-shared/v1/download.sh?_=$(date +%s)" | bash -s v1 "$shared_lib" || exit 1
+# shellcheck disable=SC1091  # Not following: $shared_lib/lib/build-image-init.sh was not specified as input
 source "$shared_lib/lib/build-image-init.sh"
 
 
@@ -19,9 +20,9 @@ source "$shared_lib/lib/build-image-init.sh"
 #################################################
 
 if [[ "${DOCKER_PUSH:-}" == "true" ]]; then
-  if ! hash regctl &>/dev/null; then
-    log ERROR "regctl (aka regclient) command line tool is misssing!"
-  fi
+   if ! hash regctl &>/dev/null; then
+      log ERROR "regctl (aka regclient) command line tool is misssing!"
+   fi
 fi
 
 
@@ -36,17 +37,19 @@ image_name=$image_repo:${DOCKER_IMAGE_TAG:-latest}
 # build the image
 #################################################
 log INFO "Building docker image [$image_name]..."
-if [[ $OSTYPE == "cygwin" || $OSTYPE == "msys" ]]; then
-  project_root=$(cygpath -w "$project_root")
+if [[ $OSTYPE == cygwin || $OSTYPE == msys ]]; then
+   project_root=$(cygpath -w "$project_root")
 fi
 
 # https://github.com/docker/buildx/#building-multi-platform-images
 set -x
 
 docker --version
-docker run --privileged --rm tonistiigi/binfmt --install all
 export DOCKER_BUILD_KIT=1
 export DOCKER_CLI_EXPERIMENTAL=1 # prevents "docker: 'buildx' is not a docker command."
+
+# Register QEMU emulators for all architectures so Docker can run and build multi-arch images
+docker run --privileged --rm tonistiigi/binfmt --install all
 
 # https://docs.docker.com/build/buildkit/configure/#resource-limiting
 echo "
@@ -56,31 +59,32 @@ echo "
 
 docker buildx version # ensures buildx is enabled
 docker buildx create --config /etc/buildkitd.toml --use # prevents: error: multiple platforms feature is currently not supported for docker driver. Please switch to a different driver (eg. "docker buildx create --use")
+# shellcheck disable=SC2154  # base_layer_cache_key is referenced but not assigned.
 docker buildx build "$project_root" \
-  --file "image/Dockerfile" \
-  --progress=plain \
-  --pull \
-  --build-arg INSTALL_SUPPORT_TOOLS=${INSTALL_SUPPORT_TOOLS:-0} \
-  `# using the current date as value for BASE_LAYER_CACHE_KEY, i.e. the base layer cache (that holds system packages with security updates) will be invalidate once per day` \
-  --build-arg BASE_LAYER_CACHE_KEY=$base_layer_cache_key \
-  --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
-  --build-arg GIT_BRANCH="${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}" \
-  --build-arg GIT_COMMIT_DATE="$(date -d @$(git log -1 --format='%at') --utc +'%Y-%m-%d %H:%M:%S UTC')" \
-  --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
-  --build-arg GIT_REPO_URL="$(git config --get remote.origin.url)" \
-  $(if [[ "${ACT:-}" == "true" || "${DOCKER_PUSH:-}" != "true" ]]; then \
-    echo -n "--load --output type=docker"; \
-  else \
-    echo -n "--platform linux/amd64,linux/arm64,linux/arm/v7"; \
-  fi) \
-  --tag $image_name \
-  $(if [[ "${DOCKER_PUSH:-}" == "true" ]]; then echo -n "--push"; fi) \
-  "$@"
+   --file "image/Dockerfile" \
+   --progress=plain \
+   --pull \
+   --build-arg "INSTALL_SUPPORT_TOOLS=${INSTALL_SUPPORT_TOOLS:-0}" \
+   `# using the current date as value for BASE_LAYER_CACHE_KEY, i.e. the base layer cache (that holds system packages with security updates) will be invalidate once per day` \
+   --build-arg BASE_LAYER_CACHE_KEY="$base_layer_cache_key" \
+   --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+   --build-arg GIT_BRANCH="${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}" \
+   --build-arg GIT_COMMIT_DATE="$(date -d "@$(git log -1 --format='%at')" --utc +'%Y-%m-%d %H:%M:%S UTC')" \
+   --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
+   --build-arg GIT_REPO_URL="$(git config --get remote.origin.url)" \
+   $(if [[ "${ACT:-}" == "true" || "${DOCKER_PUSH:-}" != "true" ]]; then \
+      echo -n "--load --output type=docker"; \
+   else \
+      echo -n "--platform linux/amd64,linux/arm64,linux/arm/v7"; \
+   fi) \
+   --tag "$image_name" \
+   $(if [[ "${DOCKER_PUSH:-}" == "true" ]]; then echo -n "--push"; fi) \
+   "$@"
 docker buildx stop
 set +x
 
 if [[ "${DOCKER_PUSH:-}" == "true" ]]; then
-  docker image pull $image_name
+   docker image pull $image_name
 fi
 
 
@@ -89,21 +93,21 @@ fi
 #################################################
 echo
 log INFO "Testing docker image [$image_name]..."
-(set -x; docker run --rm $image_name logrotate --version)
+(set -x; docker run --rm "$image_name" logrotate --version)
 echo
 
 
 #################################################
 # perform security audit
 #################################################
-if [[ "${DOCKER_AUDIT_IMAGE:-1}" == 1 ]]; then
-  bash "$shared_lib/cmd/audit-image.sh" $image_name
+if [[ ${DOCKER_AUDIT_IMAGE:-1} == 1 ]]; then
+   bash "$shared_lib/cmd/audit-image.sh" "$image_name"
 fi
 
 
 #################################################
 # push image to ghcr.io
 #################################################
-if [[ "${DOCKER_PUSH_GHCR:-}" == "true" ]]; then
-  (set -x; regctl image copy $image_name ghcr.io/$image_name)
+if [[ ${DOCKER_PUSH_GHCR:-} == true ]]; then
+   (set -x; regctl image copy "$image_name" "ghcr.io/$image_name")
 fi
